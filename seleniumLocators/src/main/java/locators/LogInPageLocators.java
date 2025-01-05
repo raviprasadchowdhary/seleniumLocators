2314c5818df1edc21e42ff9f0ff9fdd54aae0fb7
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-01-06 00:05:49 +0530

    rename

--- seleniumLocators/src/main/java/locators/LogInPageLocators.java
+++ seleniumLocators/src/main/java/locators/LogInPageLocators.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Locators {
+public class LogInPageLocators {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Script execution started!");
