cf39ea0eca16fd2fa70eb6bdb40356c41987b52b
raviprasadchowdhary <78257726+raviprasadchowdhary@users.noreply.github.com>
2025-01-07 13:25:53 +0530

    commit

---
+++ seleniumLocators/src/main/java/locators/TestExceptionsPageLocators.java
@@ -0,0 +1,56 @@
+package locators;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestExceptionsPageLocators {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Script execution started!");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		testExceptionPageLocators(driver);
+		
+		driver.quit();
+		System.out.println("Script execution completed!");
+	}
+	
+	private static void testExceptionPageLocators(WebDriver driver) throws InterruptedException {
+		driver.get("https://practicetestautomation.com/practice-test-exceptions/");
+		System.out.println(driver.getTitle());
+		Thread.sleep(500);
+		
+		// Find WebElement for the "Selenium WebDriver with Java for beginners program." link using link text
+			// store the current window handle
+			String mainWindowhandle = driver.getWindowHandle();
+			WebElement SeleniumWebDriverLinkByLinkText = driver.findElement(By.linkText("Selenium WebDriver with Java for beginners program"));
+			SeleniumWebDriverLinkByLinkText.click();
+			Thread.sleep(3000);
+			// get all window handles
+			Set<String> allWindoeHandles = driver.getWindowHandles();
+			for(String handle: allWindoeHandles) {
+				if(!handle.equals(mainWindowhandle)) {
+					driver.switchTo().window(handle);
+					break;
+				}
+			}
+			System.out.println(driver.getTitle());
+			// swithc back to main window handle
+			driver.switchTo().window(mainWindowhandle);
+			Thread.sleep(500);
+			
+		// Find WebElements for the input field using tag, class, name, XPath and CSS
+		
+		// Create a list of WebElements for all buttons using tag
+		
+		// Identify both buttons, using ID, name, CSS, XPath for each
+		
+		
+	}
+	
+}
